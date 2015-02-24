944edd7be56a200e48fbf64c57ddba13a1445e56
Connor <thecg1997@gmail.com>
2015-02-24 15:29:00 +0000

    Fixed PluginMain
    Now it's readable! :D

--- pe/nn/connor/betterbroadcast/PluginMain.java
+++ pe/nn/connor/betterbroadcast/PluginMain.java
@@ -14,24 +14,47 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginMain extends JavaPlugin{
-
+    private String tag;
+    private List<Players> players;
+    private FileConfiguration config;
+    private String colours[];
 
     private void initConfig(){
-        if(!(new File(getDataFolder(), "config.yml")).exists())
+        if(!(new File(getDataFolder(), "config.yml")).exists()){
             saveDefaultConfig();
-        tag = config.getString("tag", (new StringBuilder()).append(ChatColor.RED).append("B").append(ChatColor.GREEN).append("B&r").toString());
-        tag = (new StringBuilder("[")).append(tag).append(ChatColor.RESET).append("]").toString();
+        }
+            
+        tag = config.getString("tag", ChatColor.RED + "B" + ChatColor.GREEN + "B&r");
+        tag = "[" + tag + ChatColor.RESET + "]";
         tag = ChatColor.translateAlternateColorCodes('&', tag);
     }
 
-    public void onEnable()
-    {
+    public void onEnable(){
         players = new ArrayList();
         config = getConfig();
         colours = (new String[] {
-            (new StringBuilder()).append(ChatColor.BLACK).append("0").toString(), (new StringBuilder()).append(ChatColor.DARK_BLUE).append("1").toString(), (new StringBuilder()).append(ChatColor.DARK_GREEN).append("2").toString(), (new StringBuilder()).append(ChatColor.DARK_AQUA).append("3").toString(), (new StringBuilder()).append(ChatColor.DARK_RED).append("4").toString(), (new StringBuilder()).append(ChatColor.DARK_PURPLE).append("5").toString(), (new StringBuilder()).append(ChatColor.GOLD).append("6").toString(), (new StringBuilder()).append(ChatColor.GRAY).append("7").toString(), (new StringBuilder()).append(ChatColor.DARK_GRAY).append("8").toString(), (new StringBuilder()).append(ChatColor.BLUE).append("9").toString(), 
-            (new StringBuilder()).append(ChatColor.GREEN).append("a").toString(), (new StringBuilder()).append(ChatColor.AQUA).append("b").toString(), (new StringBuilder()).append(ChatColor.RED).append("c").toString(), (new StringBuilder()).append(ChatColor.LIGHT_PURPLE).append("d").toString(), (new StringBuilder()).append(ChatColor.YELLOW).append("e").toString(), (new StringBuilder()).append(ChatColor.WHITE).append("f").toString(), "k - MAGIC!", (new StringBuilder()).append(ChatColor.BOLD).append("l").append(ChatColor.RESET).toString(), (new StringBuilder()).append(ChatColor.STRIKETHROUGH).append("m").append(ChatColor.RESET).toString(), (new StringBuilder()).append(ChatColor.UNDERLINE).append("n").append(ChatColor.RESET).toString(), 
-            (new StringBuilder()).append(ChatColor.ITALIC).append("o").append(ChatColor.RESET).toString(), (new StringBuilder()).append(ChatColor.RESET).append("r - Resets any Colour Codes").toString()
+            ChatColor.BLACK + "0",
+            ChatColor.DARK_BLUE + "1",
+            ChatColor.DARK_GREEN + "2",
+            ChatColor.DARK_AQUA + "3",
+            ChatColor.DARK_RED + "4",
+            ChatColor.DARK_PURPLE + "5",
+            ChatColor.GOLD + "6",
+            ChatColor.GRAY + "7",
+            ChatColor.DARK_GRAY + "8",
+            ChatColor.BLUE + "9", 
+            ChatColor.GREEN + "a",
+            ChatColor.AQUA + "b",
+            ChatColor.RED + "c",
+            ChatColor.LIGHT_PURPLE + "d",
+            ChatColor.YELLOW + "e",
+            ChatColor.WHITE + "f",
+            "k - MAGIC!",
+            ChatColor.BOLD + "l" + ChatColor.RESET,
+            ChatColor.STRIKETHROUGH + "m" + ChatColor.RESET,
+            ChatColor.UNDERLINE + "n" + ChatColor.RESET, 
+            ChatColor.ITALIC + "o" + ChatColor.RESET,
+            ChatColor.RESET + "r - Resets any Colour and formatting Codes",
         });
         initConfig();
         PluginManager pm = Bukkit.getPluginManager();
@@ -41,89 +64,66 @@ public class PluginMain extends JavaPlugin{
         pm.addPermission(new Permission("bb.reload", PermissionDefault.OP));
     }
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String args[])
-    {
-        if(cmd.getName().equalsIgnoreCase("bb"))
-        {
-            if(sender.hasPermission(Bukkit.getPluginManager().getPermission("bb.broadcast")))
-            {
-                if(args.length >= 1)
-                {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]){
+        if(cmd.getName().equalsIgnoreCase("bb")){
+            if(sender.hasPermission(Bukkit.getPluginManager().getPermission("bb.broadcast"))){
+                if(args.length >= 1){
                     String bcast = "";
-                    for(int x = 0; x < args.length; x++)
-                        bcast = (new StringBuilder(String.valueOf(bcast))).append(args[x]).append(" ").toString();
-
+                    for(int x = 0; x < args.length; x++){
+                        bcast = bcast + args[x] + " ";
+                    }
+                    
                     bcast = ChatColor.translateAlternateColorCodes('&', bcast);
-                    if(!players.contains(sender.getName()))
-                        Bukkit.broadcastMessage((new StringBuilder(String.valueOf(tag))).append(" ").append(bcast).toString());
-                    else
+                    if(!players.contains(sender.getName())){
+                        Bukkit.broadcastMessage(tag + " " + bcast);
+                    }else{
                         Bukkit.broadcastMessage(bcast);
-                } else
-                {
-                    sender.sendMessage((new StringBuilder()).append(ChatColor.RED).append("What am I going to broadcast?").toString());
+                    }
+                }else{
+                    sender.sendMessage(ChatColor.RED + "What am I going to broadcast?");
                 }
-            } else
-            {
-                sender.sendMessage((new StringBuilder()).append(ChatColor.RED).append("You do not have permission!").toString());
+            }else{
+                sender.sendMessage(ChatColor.RED + "You do not have permission!");
             }
             return true;
         }
-        if(cmd.getName().equalsIgnoreCase("bbtoggle"))
-        {
-            if(sender.hasPermission(Bukkit.getPluginManager().getPermission("bb.toggle")))
-            {
-                if(!players.contains(sender.getName()))
-                {
+        if(cmd.getName().equalsIgnoreCase("bbtoggle")){
+            if(sender.hasPermission(Bukkit.getPluginManager().getPermission("bb.toggle"))){
+                if(!players.contains(sender.getName())){
                     sender.sendMessage("The tag will no longer be shown");
                     players.add(sender.getName());
-                } else
-                {
+                }else{
                     sender.sendMessage("The tag will now be shown");
                     players.remove(sender.getName());
                 }
-            } else
-            {
-                sender.sendMessage((new StringBuilder()).append(ChatColor.RED).append("You do not have permission!").toString());
+            }else{
+                sender.sendMessage(ChatColor.RED + "You do not have permission!");
             }
             return true;
         }
-        if(cmd.getName().equalsIgnoreCase("bbcolours"))
-        {
-            if(sender.hasPermission(Bukkit.getPluginManager().getPermission("bb.colours")))
-            {
+        if(cmd.getName().equalsIgnoreCase("bbcolours")){
+            if(sender.hasPermission(Bukkit.getPluginManager().getPermission("bb.colours"))){
                 String text = "";
-                String as[];
-                int j = (as = colours).length;
-                for(int i = 0; i < j; i++)
-                {
-                    String code = as[i];
-                    text = (new StringBuilder(String.valueOf(text))).append(code).append(" ").toString();
+                for(int i = 0; i < colours.length; i++){
+                    String code = colours[i];
+                    text = text + code + " ";
                 }
 
                 sender.sendMessage(text);
             }
             return true;
         }
-        if(cmd.getName().equalsIgnoreCase("bbreload"))
-        {
-            if(sender.hasPermission(Bukkit.getPluginManager().getPermission("bb.reload")))
-            {
+        if(cmd.getName().equalsIgnoreCase("bbreload")){
+            if(sender.hasPermission(Bukkit.getPluginManager().getPermission("bb.reload"))){
                 reloadConfig();
                 initConfig();
-                sender.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append(" Reloaded").toString());
-            } else
-            {
-                sender.sendMessage((new StringBuilder()).append(ChatColor.RED).append("You do not have permission!").toString());
+                sender.sendMessage(ChatColor.GREEN + "Reloaded");
+            }else{
+                sender.sendMessage(ChatColor.RED + "You do not have permission!";
             }
             return true;
-        } else
-        {
+        }else{
             return false;
         }
     }
-
-    public String tag;
-    List players;
-    FileConfiguration config;
-    String colours[];
 }
